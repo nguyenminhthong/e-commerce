108f86103e961de20e09f931c223a769cb04be09
thongnm <ngminhthong.cntp@gmail.com>
2023-03-29 23:35:02 +0700

    add new project

---
+++ NetCore/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = new JsonResult(new { message = "Hello" });
+            return await Task.FromResult(result);
+        }
+    }
+}
---
+++ NetCore/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ==================================================
+// Add services to the container.
+// ==================================================
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var services = builder.Services;
+
+services.AddStackExchangeRedisCache(configRedisCache);
+
+services.AddCors();
+
+// ==================================================
+// Configure application
+// ==================================================
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(action =>
+{
+    action.WithHeaders("*");
+    action.WithOrigins("*");
+    action.AllowAnyHeader();
+    action.AllowAnyMethod();
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+// ==================================================
+// Functionality 
+// ==================================================
+
+// Config connection for rediscache
+void configRedisCache(RedisCacheOptions options)
+{
+
+}
