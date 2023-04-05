ce9dfbd36c8cf38bca75485f4fe44011605d5328
thongnm <thongnm@allexceed.co.jp>
2023-04-05 17:57:15 +0700

    add new api

--- NetCore/Controllers/ProductController.cs
+++ NetCore/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NetCore.JsonResult;
+using NetCore.ViewModel.Categories;
 using NetCore.ViewModel.Product;
 
 namespace NetCore.Controllers
@@ -12,7 +13,7 @@ namespace NetCore.Controllers
         [HttpGet("product-feature")]
         public async Task<IActionResult> GetProductFeature()
         {
-            var products = new List<ProductFeature>() { 
+            var products = new List<ProductFeature>() {
                 new ProductFeature() { ProductId = 1, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-1.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
                 new ProductFeature() { ProductId = 2, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-2.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
                 new ProductFeature() { ProductId = 3, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-3.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
@@ -21,7 +22,7 @@ namespace NetCore.Controllers
                 new ProductFeature() { ProductId = 6, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-6.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
                 new ProductFeature() { ProductId = 7, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-7.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
                 new ProductFeature() { ProductId = 8, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-8.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20}
-            
+
             };
 
             await Task.Delay(500);
@@ -30,18 +31,7 @@ namespace NetCore.Controllers
         }
 
         [HttpGet]
-        [Route("api/products", Name ="GetProducts")]
-        public async Task<IActionResult> Index()
-        {
-            var products = new List<Product>();
-
-            await Task.Delay(500);
-
-            return new RawJsonResult<IEnumerable<Product>>(products);
-        }
-
-        [HttpGet]
-        [Route("api/productSales", Name = "GetProductSales")]
+        [Route("productSales", Name = "GetProductSales")]
         public async Task<IActionResult> GetProductSales()
         {
             var products = new List<Product>() {
@@ -59,5 +49,41 @@ namespace NetCore.Controllers
 
             return new RawJsonResult<IEnumerable<Product>>(products);
         }
+
+        [HttpGet]
+        [Route("products", Name = "GetProducts")]
+        public async Task<IActionResult> Index()
+        {
+
+            var Vegetables = new CategoryModel() { Id = 1, Title = "Vegetables", Link = "/" };
+            var FreshMeat = new CategoryModel() { Id = 2, Title = "Fresh Meat", Link = "/" };
+            var Fastfood = new CategoryModel() { Id = 3, Title = "Fastfood", Link = "/" };
+            var Oatmeal = new CategoryModel() { Id = 4, Title = "Oatmeal", Link = "/" };
+
+            var products = new List<Product>() {
+                new Product() { ProductId = 1, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-1.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
+                new Product() { ProductId = 2, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-2.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
+                new Product() { ProductId = 3, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-3.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
+                new Product() { ProductId = 4, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-4.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
+                new Product() { ProductId = 5, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-5.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
+                new Product() { ProductId = 6, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-6.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
+                new Product() { ProductId = 7, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-7.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20},
+                new Product() { ProductId = 8, ProductName="Crab Pool Security", ProductImage = "/assets/upload/featured/feature-8.jpg", Price = 6500000, HasSale= true, SalePrice = 350000, SalePer= 20}
+            };
+
+            var productPages = new List<ProductPage>()
+            {
+                new ProductPage() {Category =  Vegetables, Products = products},
+                new ProductPage() {Category =  FreshMeat, Products = products},
+                new ProductPage() {Category =  Fastfood, Products = products},
+                new ProductPage() {Category =  Oatmeal, Products = products}
+
+            };
+
+            await Task.Delay(500);
+
+            return new RawJsonResult<IEnumerable<ProductPage>>(productPages);
+        }
+
     }
 }
---
+++ NetCore/ViewModel/Product/ProductPage.cs
@@ -0,0 +1,11 @@
+﻿using NetCore.ViewModel.Categories;
+
+namespace NetCore.ViewModel.Product
+{
+    public record ProductPage
+    {
+        public CategoryModel Category { get; set; }
+
+        public IList<Product> Products { get; set; }
+    }
+}
